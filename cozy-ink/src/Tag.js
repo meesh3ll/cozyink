a7b724b99d68e3e1514a3a1580344badd929776c
meesh3ll <wanmichelle0@gmail.com>
2023-08-26 16:06:41 -0700

    gottem

--- cozy-ink/src/Tag.js
+++ cozy-ink/src/Tag.js
@@ -1,7 +1,7 @@
 const Tag = ({ tag, handleTagChannel }) => {
     return (
-        <div className="tag">
-            <button onClick={handleTagChannel} value={tag}>{tag}</button>
+        <div className = "tag">
+            <button className = "tag-channel" onClick={handleTagChannel} value={tag}>{tag}</button>
         </div>
     );
 };
--- cozy-ink/src/TagList.js
+++ cozy-ink/src/TagList.js
@@ -1,13 +1,15 @@
 import {Tag} from './Tag'
+import {MdDelete} from 'react-icons/md'
+import {MdHome} from 'react-icons/md'
 
 const TagList = ({ tagList, handleTagChannel }) => {
     return (
-        <div style={{background: "lightblue"}}>
-            <button onClick={handleTagChannel} value="">Home</button>
+        <div>
+            <button className = "tag-channel" onClick={handleTagChannel} value=""><MdHome size = "1.6em"/></button>
             {Array.from(tagList).map((tag) => (
                 <Tag tag={tag} handleTagChannel={handleTagChannel}/>
             ))}
-            <button onClick={handleTagChannel} value="deleted">Trash</button>
+            <button onClick={handleTagChannel} value="deleted" className = "tag-channel"><MdDelete size = "1.6em"/></button>
         </div>
     );
 };
