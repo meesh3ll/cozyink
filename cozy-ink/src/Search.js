ef11f4b91af7732a9978c8e8426344523e3fa802
meesh3ll <wanmichelle0@gmail.com>
2023-08-25 21:50:11 -0700

    saving

--- cozy-ink/src/Search.js
+++ cozy-ink/src/Search.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import {MdSearch} from 'react-icons/md';
 
-const SearchBar = () => {
+const SearchBar = ({handleSearch}) => {
     return(
         <div className = "search-bar">
             <MdSearch className = "search-icons" size = '1.2em'/>
-            <input type = "text" placeholder = "Search for notes..." />
+            <input onChange = {(event) => handleSearch(event.target.value)} type = "text" placeholder = "Search for notes..." />
         </div>
     );
 }
