186e1a9dd9f5130a8ca2f3fd254a638c997048b3
yureinon <klam11504@gmail.com>
2023-08-26 00:13:32 -0700

    got text boxes working and saving + other issues

--- cozy-ink/src/AddNote.js
+++ cozy-ink/src/AddNote.js
@@ -3,16 +3,19 @@ import { useState } from 'react';
 const AddNote = ({id, title, text, tag, handleAddNote}) => {
     const [noteText, setNoteText] = useState('');
     const [titleText, setTitleText] = useState('');
-    const handleChange = (event) => {
+    const handleNoteTextChange = (event) => {
         setNoteText(event.target.value);
     }
+    const handleTitleChange = (event) => {
+        setTitleText(event.target.value);
+    }
     const handleSave = (event) => {
-        handleAddNote(noteText, titleText);
+        handleAddNote(titleText, noteText);
     }
     return (
         <><div className='new note'>
-            <textarea rows='1' cols='1' placeholder='title...' value={titleText} onChange={handleChange}></textarea>
-            <textarea rows='10' cols='10' placeholder='type...' value={noteText} onChange={handleChange}></textarea>
+            <textarea rows='1' cols='1' placeholder='title...' value={titleText} onChange={handleTitleChange}></textarea>
+            <textarea rows='10' cols='10' placeholder='type...' value={noteText} onChange={handleNoteTextChange}></textarea>
             <div className='footer'>
                 <small>{tag}</small>
                 <button className='save' onClick={handleSave}></button>
