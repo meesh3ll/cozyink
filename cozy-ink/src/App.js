4d821d31814bf9587787ed8cfb3e7ab844e0b164
pineapplezsauce <hayleydlin@gmail.com>
2023-08-25 19:26:43 -0700

    import functions app.js; created function tag.js

--- cozy-ink/src/App.js
+++ cozy-ink/src/App.js
@@ -1,4 +1,8 @@
 import './App.css';
+import {Taglist} from './Tag'
+import {noteButton} from './Search'
+import {searchBar} from './Search'
+import {noteSpace} from './Note'
 
 function App() {
   return (
