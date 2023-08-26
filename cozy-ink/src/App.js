e7641cf7e418b7c82dfb450fef41409f0aa34e19
yureinon <klam11504@gmail.com>
2023-08-25 21:29:05 -0700

    idek

--- cozy-ink/src/App.js
+++ cozy-ink/src/App.js
@@ -2,29 +2,12 @@ import './App.css';
 import {TagList} from './Tag'
 import {NoteButton} from './Search'
 import {SearchBar} from './Search'
-import {NotesList} from './NotesList'
-import {useState} from 'react';
+import {Note} from './Note'
 
 function App() {
   return (
     <div>
-    
-      <div>
-        <TagList/>
-      </div>
-
-      <div>
-        <SearchBar/>
-      </div>
-
-      <div>
-        <NotesList/>
-      </div>
-
-      <div>
-        <NoteButton/>
-      </div>
-
+      <TagList />
     </div>
   );
 }
--- cozy-ink/src/NotesList.js
+++ cozy-ink/src/NotesList.js
@@ -3,6 +3,7 @@ import Note from './Note';
 const NotesList = ({ notes }) => {
     return (
         <div className='notes-list'>
+            <Note/>
         </div>
     )
 }
