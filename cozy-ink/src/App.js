ebda7b95ba7aafc9423ea60ed9d8c1cd24b4d277
meesh3ll <wanmichelle0@gmail.com>
2023-08-25 21:24:55 -0700

    lmao

--- cozy-ink/src/App.js
+++ cozy-ink/src/App.js
@@ -1,8 +1,21 @@
 import './App.css';
+import {useState} from 'react';
+import {SearchBar} from './Search';
+import {NoteButton} from './NoteButton';
 
 function App() {
+  const [searchInfo, setSearch] 
   return (
+    <div>
+      <div>
+        <SearchBar />
+      </div>
 
+      <div>
+        <NoteButton />
+      </div>
+    </div>
+  
   );
 }
 
