a202ebc8fd4a4417a0f26b95222fde99c6fb663c
pineapplezsauce <hayleydlin@gmail.com>
2023-08-26 15:15:10 -0700

    all working now :]

--- cozy-ink/src/App.js
+++ cozy-ink/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { nanoid } from 'nanoid';
 import { TagList } from './TagList';
 import { NoteButton } from './Search';
@@ -10,13 +10,11 @@ function App() {
   const [notes, setNotes] = useState([]);
 
   const [tagList, setTagList] = useState(() => new Set());
-
-  const addNote = (title, text, tags) => {
-    setTagList(prev => new Set(prev).add(tags));
     
   const [searchInfo, setSearch] = useState('');
     
   const addNote = (title, text, tag) => {
+    setTagList(prev => new Set(prev).add(tag));
     const newNote = {
       id: nanoid(),
       title: title,
@@ -65,10 +63,6 @@ function App() {
       );
     }
   };
-
-  console.log(tagList);
-  console.log(notes);
-  //console.log(notes.filter(note => !tagList.has(note.tag)));
 
   return (
     <div>
